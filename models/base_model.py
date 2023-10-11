42057a7a09e81a9edc1eea7b8a4ee08d2f0623be
victorchiemeka <victorchukwuemeka127@gmail.com>
2023-10-11 12:14:46 +0100

    conflict merged

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     Represents a base model with common attributes and methods.
@@ -9,32 +10,30 @@ class BaseModel:
         self (BaseModel): The current instance.
     """
 
-
     def __init__(self, *args, **kwargs):
-            """
-            Initializes a new instance of the BaseModel class.
+        """
+        Initializes a new instance of the BaseModel class.
 
-                    This constructor initializes the common attributes for all derived
-                    classes. If keyword arguments are provided, it populates the instance's
-                    attributes based on the key-value pairs in kwargs.
+                This constructor initializes the common attributes for all derived
+                classes. If keyword arguments are provided, it populates the instance's
+                attributes based on the key-value pairs in kwargs.
 
-            Args:
-                    *args: Not used in this method.
-                    **kwargs (dict): Dictionary of key-value pairs representing attributes.
+        Args:
+                *args: Not used in this method.
+                **kwargs (dict): Dictionary of key-value pairs representing attributes.
 
-            Public Attributes:
-                    id (str): A unique identifier generated for the instance.
-                    created_at (datetime): The timestamp when the instance was created.
-                    updated_at (datetime): The timestamp when the instance was last updated.
+        Public Attributes:
+                id (str): A unique identifier generated for the instance.
+                created_at (datetime): The timestamp when the instance was created.
+                updated_at (datetime): The timestamp when the instance was last updated.
 
-            Raises:
-                    ValueError: If an invalid date format is provided in `kwargs`.
+        Raises:
+                ValueError: If an invalid date format is provided in `kwargs`.
 
-            Returns:
-                    None
-            """
-               self.created_at = datetime.today()
-               self.updated_at = datetime.today()
-               self.id = str(uuid.uuid4())
-               # TODO convert datetime to iso format
-
+        Returns:
+                None
+        """
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        self.id = str(uuid.uuid4())
+        # TODO convert datetime to iso format
