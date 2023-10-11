894e3803f9d246af42f3b4907322faa90a4507c3
bazileros <zalisilesithule@gmail.com>
2023-10-11 15:44:24 +0200

    implemented def save and def to dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 import uuid
 from datetime import datetime
 import models
-from models.engine import storage
 
 
 class BaseModel:
@@ -41,16 +40,53 @@ class BaseModel:
         #     attr = self.__dict__
         #     return f"[{Class}] ({object_id}) {attr}"
 
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        self.id = str(uuid.uuid4())
+        if len(**kwargs):
+            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                        if key in ["created_at"], ["updated_at"]:
+                                self.__dict__[key] = datetime.strip(key, value)
+            
+
 
         def save(self):
-            self.updated_at = datetime()
-            storage.save()
+            self.updated_at = datetime.today()
+            models.storage.save()
+
 
         def to_dict(self):
-            self.__dict__.copy()
+            result = {
+                "_class": self.__class.__name_,
+                "id": self.id,
+                "created_at": self.created_at.isoformat(),
+                "updated_at": self.updated_at.isoformat(),
+            }
+            result.update(self._dict_)  # Add instance attributes
+            return result
+        
 
         def __str__(self):
-            return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+            return f"[{self.__class__.__name}] ({self.id}) {self.__dict}"
+# TODO refactor this code into our implementation
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        # Initialize created_at and updated_at with current timestamps
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        
+        # If 'created_at' or 'updated_at' are provided in kwargs, update the attributes
+        if 'created_at' in kwargs:
+            self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+        if 'updated_at' in kwargs:
+            self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+        
+        # You can remove other items from kwargs here if needed
+        # kwargs.pop('created_at', None)
+        # kwargs.pop('updated_at', None)
+        
+        # Store any remaining attributes in the instance
+        self.__dict__.update(kwargs)
