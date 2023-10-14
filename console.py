747b72b31ead76eb42f26208dddc19d892cff269
bazileros <zalisilesithule@gmail.com>
2023-10-14 20:51:06 +0200

    console

--- console.py
+++ console.py
@@ -16,62 +16,67 @@ from models.place import Place
 from models.base_model import BaseModel
 
 
-# def parse(arg):
-#     """
-#     Method written to take and parse input before use 
-#     """
-#     curly_braces = re.search(r"\{(.*?)\}", arg)
-#     brackets = re.search(r"\[(.*?)\]", arg)
-#     if curly_braces is None:
-#         if brackets is None:
-#             return [i.strip(",") for i in split(arg)]
-#         else:
-#             lexer = split(arg[:curly_braces.span()[0]])
-#             rlt = [i.strip() for i in lexer]
-#             rlt.append(brackets.group())
-#             return rlt
-#     else:
-#         lexer = split(arg[:curly_braces.span()[0]])
-#         rlt = [i.strip() for i in lexer]
-#         rlt.append(curly_braces.group())
-#         return rlt
-
-
-
 def parse(arg):
     """
     Method to parse and process input
     """
+
+    # Search for curly braces and square brackets in the input
     curly_braces_match = re.search(r"\{(.*?)\}", arg)
     brackets_match = re.search(r"\[(.*?)\]", arg)
-    
-    # Use shlex.split to split the input string into tokens
+
+    # Split the input string into tokens using shlex
     tokens = shlex.split(arg)
-    
-    if curly_braces_match is None:
-        if brackets_match is None:
-            # If neither curly braces nor brackets are found,
-            #  return the tokens
-            return tokens
-        else:
-            # Split the tokens before the curly braces,
-            # strip extra whitespace, and add the brackets
-            # to the result
-            before_curly_braces = tokens[:brackets_match.start()]
-            result = [item.strip() for item in before_curly_braces]
-            result.append(brackets_match.group())
-            return result
-    else:
-        # Split the tokens before the curly braces,
-        #  strip extra whitespace,
-        #  and add the curly braces to the result
-        before_curly_braces = tokens[:curly_braces_match.start()]
-        result = [item.strip() for item in before_curly_braces]
-        result.append(curly_braces_match.group())
+
+    # Case 1: Neither curly braces nor brackets are found
+    if curly_braces_match is None and brackets_match is None:
+        return tokens
+
+    # Case 2: Only square brackets are found
+    if brackets_match:
+        before_brackets = tokens[:brackets_match.start()]
+        result = [item.strip() for item in before_brackets]
+        result.append(brackets_match.group())
         return result
+
+    # Case 3: Only curly braces are found
+    before_curly_braces = tokens[:curly_braces_match.start()]
+    result = [item.strip() for item in before_curly_braces]
+    result.append(curly_braces_match.group())
+    return result
+
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand - A command-line interface for interacting with HBNB data.
+
+    This class provides a command-line interface for creating and managing instances
+    of different classes in the HBNB data storage system. It supports commands for
+    creating, showing, exiting, and handling end-of-file events.
+
+    Attributes:
+        prompt (str): The command prompt for the shell.
+        __classes (set): A set containing the names of available classes.
+
+    Public Methods:
+        emptyline(self): Override of the default method to do nothing when the user
+        presses Enter with an empty input.
+
+        do_quit(self, arg): Command to exit the shell. Returns True to exit.
+
+        do_exit(self, arg): Command to exit the shell. Returns True to exit.
+
+        do_EOF(self, arg): Handles the End of File (EOF) event. Prints a newline and calls
+        the `do_exit` method to exit the shell.
+
+        do_create(self, arg): Creates an instance of a class based on the provided class name.
+
+        do_show(self, arg): Displays the string representation of a class instance.
+
+    Example:
+        To create an instance of the 'BaseModel' class, you can enter: `create BaseModel`
+    """
     
     prompt = "(hbnb) "
     __classes = {
@@ -85,39 +90,60 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def emptyline(self):
+        """ Called when an empty line is entered."""
         pass
     
     
     def do_quit(self, arg):
-        """Exit the shell."""
+        """ Exit the shell."""
         return True
     
     
     def do_exit(self, arg):
+        """ Exit the shell """
         return True
     
     def do_EOF(self, arg):
+        """
+        End of File event [EOF]
+
+        Args:
+            arg (string): Ignored. This method does not expect any arguments.
+        """
         print = ""
         self.do_exit()
 
+    
     def do_create(self, arg):
         """
-        create instance of a class
+        Create an instance of a class
+
+        args:
+            arg (str): A string containing the class name
+            for which an instance should be created.
         """
         args = parse(arg)
+
         if len(args) == 0:
             print("** Class name is missing **")
         elif args[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
+            print("** Class doesn't exist **")
         else:
-            print(eval(arg[0])().id)
+            # If Valid class name, create an instance and print its ID
+            class_name = args[0]
+            instance = eval(class_name)()
+            print(instance.id)
             storage.save()
+
 
 
     def do_show(self, arg):
         """
          show <class> <id>
             Display string representation of the class instance
+        Args:
+            arg (str): A string containing the class name and instance ID.
+
         """
         args = parse(arg)
         objdict = storage.all()
@@ -128,7 +154,7 @@ class HBNBCommand(cmd.Cmd):
             "class_not_exist": lambda: print("** class doesn't exist **"),
             "instance_id_missing": lambda: print("** instance id is missing **"),
             "instance_not_found": lambda: print("** no instance found **"),
-            "valid_show": lambda: print(objdict["{}.{}".format(args[0], args[1])])
+            "valid_arg": lambda: print(objdict["{}.{}".format(args[0], args[1])])
         }
 
 
@@ -141,10 +167,44 @@ class HBNBCommand(cmd.Cmd):
         elif f"{args[0]}.{args[1]}" not in objdict:
             command_case = "instance_not_found"
         else:
-            command_case = "valid_show"
+            command_case = "valid_arg"
 
         command_cases[command_case]()
 
+def do_destroy(self, arg):
+        args = parse(arg)
+        objdict = storage.all()
+
+ 
+        command_cases = {
+            "class_name_missing": lambda: print("** class name missing **"),
+            "class_not_exist": lambda: print("** class doesn't exist **"),
+            "instance_id_missing": lambda: print("** instance id is missing **"),
+            "instance_not_found": lambda: print("** no instance found **"),
+            "delete_and_save": lambda: self.delete_and_save(objdict, args[0], args[1])
+        }
+
+
+        if len(args) == 0:
+            command_case = "class_name_missing"
+        elif args[0] not in HBNBCommand.__classes:
+            command_case = "class_not_exist"
+        elif len(args) == 1:
+            command_case = "instance_id_missing"
+        elif f"{args[0]}.{args[1]}" not in objdict:
+            command_case = "instance_not_found"
+        else:
+            command_case = "delete_and_save"
+
+        command_cases[command_case]()
+
+def delete_and_save(self, objdict, class_name, instance_id):
+    """Delete the instance from the storage and save the changes."""
+    del objdict["{}.{}".format(class_name, instance_id)]
+    storage.save()
+
+
+def do_all(self, arg):
 
 
 
@@ -153,5 +213,7 @@ class HBNBCommand(cmd.Cmd):
 
 
 
-if __name__ == '__main__':
+
+
+ if __name__ == '__main__':
     HBNBCommand().cmdloop()
